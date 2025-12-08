a3f1fcde8b07478b6de398f0eb41ec604702f708
Abdallah Emam <abdallah@betaedgetech.com>
2025-12-08 23:21:10 +0400

    Fix: Exclude static files from middleware to resolve 404s

--- middleware.ts
+++ middleware.ts
@@ -40,12 +40,12 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     /*
-     * Match all request paths except:
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public files (public folder)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 }
